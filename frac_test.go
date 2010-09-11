a48e685546d9c0479fe0b18091de76f2dc92cc27
Anschel Schaffer-Cohen <anschelsc@gmail.com>
2010-09-11 00:35:49 -0400

    Added tests for Add and Mult

--- frac_test.go
+++ frac_test.go
@@ -39,14 +39,14 @@ func TestNew(t *testing.T) {
 		t.Errorf("Expected positive, got negative")
 	}
 
-	f,err=New(14,7)
-	if err!=nil{
+	f, err = New(14, 7)
+	if err != nil {
 		t.Errorf("New(14, 7) returned error %v, expected nil", err)
 	}
-	if num:=f.Numerator(); num!=2{
+	if num := f.Numerator(); num != 2 {
 		t.Errorf("Expected numerator 2, got %v", num)
 	}
-	if den:=f.Denominator(); den!=1{
+	if den := f.Denominator(); den != 1 {
 		t.Errorf("Expected denominator 1, got %v", den)
 	}
 	if !f.Positive() {
@@ -70,5 +70,74 @@ func TestNew(t *testing.T) {
 	}
 	if f.Positive() {
 		t.Errorf("Expected negative, got positive")
+	}
+
+	f, err = New(3, -3)
+	if err != nil {
+		t.Errorf("New(3, -3) returned error %v, expected nil", err)
+	}
+	if num := f.Numerator(); num != -1 {
+		t.Errorf("Expected numerator -1, got %v", num)
+	}
+	if den := f.Denominator(); den != 1 {
+		t.Errorf("Expected denominator 1, got %v", den)
+	}
+	if f.Positive() {
+		t.Errorf("Expected negative, got positive")
+	}
+}
+
+func TestMult(t *testing.T) {
+	positive, _ := New(1, 2)
+	negative, _ := New(-1, 2)
+	if s := positive.Times(positive).String(); s != "1/4" {
+		t.Errorf("(1/2) * (1/2) = %v", s)
+	}
+	if s := negative.Times(positive).String(); s != "-1/4" {
+		t.Errorf("(-1/2) * (1/2) = %v", s)
+	}
+	if s := positive.Times(negative).String(); s != "-1/4" {
+		t.Errorf("(1/2) * (-1/2) = %v", s)
+	}
+	if s := negative.Times(negative).String(); s != "1/4" {
+		t.Errorf("(-1/2) * (-1/2) = %v", s)
+	}
+
+	third, _ := New(1, 3)
+	fourFifths, _ := New(4, 5)
+	nine, _ := New(9, 1)
+	if s := third.Times(fourFifths).String(); s != "4/15" {
+		t.Errorf("(1/3) * (4/5) = %v", s)
+	}
+	if s := fourFifths.Times(nine).String(); s != "36/5" {
+		t.Errorf("(4/5) * (9/1) = %v", s)
+	}
+	if s := third.Times(nine).String(); s != "3/1" {
+		t.Errorf("(1/3) * (9/1) = %v", s)
+	}
+}
+
+func TestAddAndNeg(t *testing.T) {
+	half, _ := New(1, 2)
+	third, _ := New(1, 3)
+	minusHalf := half.Negative()
+	minusThird := third.Negative()
+	if s := minusHalf.String(); s != "-1/2" {
+		t.Errorf("-(1/2) = %v", s)
+	}
+	if s := minusThird.String(); s != "-1/3" {
+		t.Errorf("-(1/3) = %v", s)
+	}
+	if s:=half.Plus(third).String();s!="5/6"{
+		t.Errorf("(1/2) + (1/3) = %v",s)
+	}
+	if s:=half.Plus(minusThird).String();s!="1/6"{
+		t.Errorf("(1/2) - (1/3) = %v",s)
+	}
+	if s:=minusHalf.Plus(third).String(); s!="-1/6"{
+		t.Errorf("-(1/2) + (1/3) = %v",s)
+	}
+	if s:=minusHalf.Plus(minusThird).String();s!="-5/6"{
+		t.Errorf("-(1/2) - (1/3) = %v",s)
 	}
 }
